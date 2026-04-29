7892ceeb2e11edaff7fc44f80754d90ed1a777e1
Kiam Venter <kiam.venter@netsweeper.com>
2026-04-29 20:12:33 +0200

    Adjusted file pos

--- include.js
+++ include.js
@@ -1,10 +1,11 @@
 function loadHTML(id, file) {
-  fetch(file)
+  fetch(SITE_ROOT + "/header-footer/" + file)
     .then(response => response.text())
     .then(data => {
       document.getElementById(id).innerHTML = data;
     });
 }
 
+const SITE_ROOT = "";
 loadHTML("header", "header-footer/header.html");
 loadHTML("footer", "header-footer/footer.html");
