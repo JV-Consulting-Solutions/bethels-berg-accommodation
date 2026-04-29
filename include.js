d0f25f53099f7ecb13617089604a2033671ff725
Kiam Venter <kiam.venter@netsweeper.com>
2026-04-29 18:29:33 +0200

    File extension changes

--- include.js
+++ include.js
@@ -6,5 +6,5 @@ function loadHTML(id, file) {
     });
 }
 
-loadHTML("header", "/header-footer/header.html");
-loadHTML("footer", "/header-footer/footer.html");
+loadHTML("header", "header-footer/header.html");
+loadHTML("footer", "header-footer/footer.html");
