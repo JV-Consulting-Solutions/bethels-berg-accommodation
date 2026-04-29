ffed7e5156f5b78eb0fcc3329dbc0ebb68dd20a2
Kiam Venter <kiam.venter@netsweeper.com>
2026-04-29 18:06:17 +0200

    Added all necessary files to project

---
+++ include.js
@@ -0,0 +1,10 @@
+function loadHTML(id, file) {
+  fetch(file)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+    });
+}
+
+loadHTML("header", "/header-footer/header.html");
+loadHTML("footer", "/header-footer/footer.html");
