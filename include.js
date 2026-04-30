97d04daa906b978bc1fd5d7309afcf06061ae4e8
Kiam Venter <kiam.venter@netsweeper.com>
2026-04-30 10:47:20 +0200

    Adjusted file pos

--- include.js
+++ include.js
@@ -1,11 +1,10 @@
 function loadHTML(id, file) {
-  fetch(SITE_ROOT + "/header-footer/" + file)
+  fetch(file)
     .then(response => response.text())
     .then(data => {
       document.getElementById(id).innerHTML = data;
     });
 }
 
-const SITE_ROOT = "/bethels-berg-accommodation/";
 loadHTML("header", "header-footer/header.html");
 loadHTML("footer", "header-footer/footer.html");
